1cea1778f57703cac6e59e4f0ecb8396e11621fa
dramaticking99 <cksanwariya6@gmail.com>
2025-08-05 20:54:03 +0530

    add missing files

---
+++ web summary/repo/src/components/LogoGradient.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+
+const LogoGradient = ({ className = "", width = "100%", height = "100%" }) => {
+  return (
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      width={width} 
+      height={height} 
+      viewBox="0 0 944.88 566.93"
+      className={className}
+    >
+      <defs>
+        <linearGradient id="grad1" x1="141.73" y1="453.31" x2="141.73" y2="169.85" gradientUnits="userSpaceOnUse">
+          <stop offset="0" style={{stopColor:"#BF1B2C"}}/>
+          <stop offset="1" style={{stopColor:"#ED1C24"}}/>
+        </linearGradient>
+        <linearGradient id="grad2" x1="141.73" y1="453.31" x2="141.73" y2="169.85" gradientUnits="userSpaceOnUse">
+          <stop offset="0" style={{stopColor:"#971421"}}/>
+          <stop offset="1" style={{stopColor:"#BA151B"}}/>
+        </linearGradient>
+        <linearGradient id="grad3" x1="283.46" y1="471.25" x2="283.46" y2="93.3" gradientUnits="userSpaceOnUse">
+          <stop offset="0" style={{stopColor:"#ED1C24"}}/>
+          <stop offset="1" style={{stopColor:"#EF2E32"}}/>
+        </linearGradient>
+        <linearGradient id="grad4" x1="283.46" y1="471.25" x2="283.46" y2="93.3" gradientUnits="userSpaceOnUse">
+          <stop offset="0" style={{stopColor:"#BA151B"}}/>
+          <stop offset="1" style={{stopColor:"#BB2326"}}/>
+        </linearGradient>
+        <linearGradient id="grad5" x1="472.44" y1="473.36" x2="472.44" y2="95.41" gradientUnits="userSpaceOnUse">
+          <stop offset="0" style={{stopColor:"#EF2E32"}}/>
+          <stop offset="1" style={{stopColor:"#F14624"}}/>
+        </linearGradient>
+        <linearGradient id="grad6" x1="472.44" y1="473.36" x2="472.44" y2="95.41" gradientUnits="userSpaceOnUse">
+          <stop offset="0" style={{stopColor:"#BB2326"}}/>
+          <stop offset="1" style={{stopColor:"#BC361B"}}/>
+        </linearGradient>
+        <linearGradient id="grad7" x1="661.42" y1="461.75" x2="661.42" y2="83.8" gradientUnits="userSpaceOnUse">
+          <stop offset="0" style={{stopColor:"#F14624"}}/>
+          <stop offset="1" style={{stopColor:"#F68712"}}/>
+        </linearGradient>
+        <linearGradient id="grad8" x1="661.42" y1="461.75" x2="661.42" y2="83.8" gradientUnits="userSpaceOnUse">
+          <stop offset="0" style={{stopColor:"#BC361B"}}/>
+          <stop offset="1" style={{stopColor:"#C0680E"}}/>
+        </linearGradient>
+        <linearGradient id="grad9" x1="803.15" y1="282.04" x2="803.15" y2="-1.42" gradientUnits="userSpaceOnUse">
+          <stop offset="0" style={{stopColor:"#F68712"}}/>
+          <stop offset="1" style={{stopColor:"#F9A72B"}}/>
+        </linearGradient>
+      </defs>
+      
+      <g>
+        <clipPath id="clip1">
+          <path d="M141.73,425.2 L0,283.47 94.49,0 188.98,283.47 141.73,425.2"/>
+        </clipPath>
+        <path d="M141.73,425.2 L0,283.47 94.49,0 188.98,283.47 141.73,425.2" fill="url(#grad1)"/>
+      </g>
+      
+      <g>
+        <clipPath id="clip2">
+          <path d="M94.49,566.93L94.49,566.93L0,283.47 141.73,425.2 94.49,566.93"/>
+        </clipPath>
+        <path d="M94.49,566.93L94.49,566.93L0,283.47 141.73,425.2 94.49,566.93" fill="url(#grad2)"/>
+      </g>
+      
+      <g>
+        <clipPath id="clip3">
+          <path d="M283.46,566.93L94.49,566.93L346.46,188.98 377.95,283.47 283.46,566.93"/>
+        </clipPath>
+        <path d="M283.46,566.93L94.49,566.93L346.46,188.98 377.95,283.47 283.46,566.93" fill="url(#grad3)"/>
+      </g>
+      
+      <g>
+        <clipPath id="clip4">
+          <path d="M94.49,566.93L94.49,566.93L283.46,0L472.44,0 283.46,0L346.46,188.98 94.49,566.93"/>
+        </clipPath>
+        <path d="M94.49,566.93L94.49,566.93L283.46,0L472.44,0 283.46,0L346.46,188.98 94.49,566.93" fill="url(#grad4)"/>
+      </g>
+      
+      <g>
+        <clipPath id="clip5">
+          <path d="M535.43,377.96 L283.46,0l188.98,-0l94.49,283.47 -31.5,94.49"/>
+        </clipPath>
+        <path d="M535.43,377.96 L283.46,0l188.98,-0l94.49,283.47 -31.5,94.49" fill="url(#grad5)"/>
+      </g>
+      
+      <g>
+        <clipPath id="clip6">
+          <path d="M472.44,566.93L472.44,566.93L346.46,188.98 283.46,0 535.43,377.96 472.44,566.93"/>
+        </clipPath>
+        <path d="M472.44,566.93L472.44,566.93L346.46,188.98 283.46,0 535.43,377.96 472.44,566.93" fill="url(#grad6)"/>
+      </g>
+      
+      <g>
+        <clipPath id="clip7">
+          <path d="M661.42,566.93L472.44,566.93L850.39,0 661.42,566.93"/>
+        </clipPath>
+        <path d="M661.42,566.93L472.44,566.93L850.39,0 661.42,566.93" fill="url(#grad7)"/>
+      </g>
+      
+      <g>
+        <clipPath id="clip8">
+          <path d="M472.44,566.93L472.44,566.93L661.42,0L850.39,0L472.44,566.93"/>
+        </clipPath>
+        <path d="M472.44,566.93L472.44,566.93L661.42,0L850.39,0L472.44,566.93" fill="url(#grad8)"/>
+      </g>
+      
+      <g>
+        <clipPath id="clip9">
+          <path d="M944.88,283.47 L850.39,0L661.42,0Z"/>
+        </clipPath>
+        <path d="M944.88,283.47 L850.39,0L661.42,0Z" fill="url(#grad9)"/>
+      </g>
+    </svg>
+  );
+};
+
+export default LogoGradient; 
